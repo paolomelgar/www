ffe84a663bd5a4f659e3ff9062572bd30a65e316
paolomelgar <amp.paolo@gmail.com>
2018-11-15 12:22:51 -0500

    Ferreboom

--- neofer/caja/producto.php
+++ neofer/caja/producto.php
@@ -20,7 +20,7 @@ require_once('../connection.php');
         $diff=date_diff(date_create(date('Y-m-d')),date_create($row['antiguedad']));?>  
     ?>
       <tr class="tr" <?php if($diff->format("%a")>=365){echo "bgcolor='#FF8B8B'";}else if($diff->format("%a")<365 && $diff->format("%a")>=182){echo "bgcolor='#FBFE33'";}else{echo "bgcolor='white'";}?> style='font-weight: bold'>
-        <td style='padding:0px;' title='s'><?php echo '<img src="https://raw.githubusercontent.com/paolomelgar/www/master/huanuco/fotos/producto/a'.$row['codigo'].'.jpg?timestamp=23124" width="100%" height="100%">'; ?></td>
+        <td style='padding:0px;' title='s'><?php echo '<img src="https://raw.githubusercontent.com/paolomelgar/www/master/neofer/fotos/producto/a'.$row['codigo'].'.jpg?timestamp=23124" width="100%" height="100%">'; ?></td>
         <td style='display:none;'><?php echo $row['id']; ?></td>
         <td><?php echo $row['producto']; ?></td>
         <td><?php echo $row['marca']; ?></td>
